fd7e8cd76aefb92fb0979e0c06639f9ef3f86c46
Samuel Seve <samuelseve1@gmail.com>
2022-08-26 23:20:21 -0300

    logbox

---
+++ Trilha 2022/Módulo 1 - Fundamentos/imhere/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text } from 'react-native'
+
+export default function App() {
+  return (
+    <>
+      <Text>Samu</Text>
+      <StatusBar style="dark" />
+    </>
+  )
+}
