26ffc21687ff10b5f9b49399ce0d020bdc673de6
Nihilboy1 <samuelseve1@gmail.com>
2022-09-21 08:14:45 -0300

    botão condicional

--- Trilha 2022/Módulo 2 - Criando Interfaces e Navegação/igniteteams/src/components/Header/index.tsx
+++ Trilha 2022/Módulo 2 - Criando Interfaces e Navegação/igniteteams/src/components/Header/index.tsx
@@ -1,8 +1,18 @@
 import logoImage from '@assets/logo.png'
-import { Container, Logo } from './styles'
-export function Header() {
+import { BackButton, BackIcon, Container, Logo } from './styles'
+
+interface HeaderProps {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImage} />
     </Container>
   )
