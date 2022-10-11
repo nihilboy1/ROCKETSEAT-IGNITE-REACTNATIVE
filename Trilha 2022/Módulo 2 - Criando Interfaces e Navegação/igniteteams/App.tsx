8f7e167dbd332d642331eb7b2a0b56dd0589a11c
Nihilboy1 <samuelseve1@gmail.com>
2022-10-11 13:35:48 -0300

    useTheme

--- Trilha 2022/Módulo 2 - Criando Interfaces e Navegação/igniteteams/App.tsx
+++ Trilha 2022/Módulo 2 - Criando Interfaces e Navegação/igniteteams/App.tsx
@@ -4,7 +4,7 @@ import {
   Roboto_700Bold,
   useFonts
 } from '@expo-google-fonts/roboto'
-import { Groups } from '@screens/Groups'
+import { NewGroup } from '@screens/NewGroup'
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
@@ -13,8 +13,12 @@ export default function App() {
   const [fontsHasBeenLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsHasBeenLoaded ? <Groups /> : <Loading />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsHasBeenLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   )
 }
