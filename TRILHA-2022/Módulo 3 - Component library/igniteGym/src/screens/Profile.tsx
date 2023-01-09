8c558f81946ee1a1379c159660f9ab0bc75fe632
nihilboy1 <samuelseve1@gmail.com>
2023-01-09 15:46:00 -0300

    Finalização das aulas sobre upload de fotos

--- TRILHA-2022/Módulo 3 - Component library/igniteGym/src/screens/Profile.tsx
+++ TRILHA-2022/Módulo 3 - Component library/igniteGym/src/screens/Profile.tsx
@@ -9,38 +9,89 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
+
+import * as ImagePicker from "expo-image-picker";
+
 const PHOTO_SIZE = 32;
 
 export function Profile() {
   const [avatarIsLoading, setAvatarIsLoading] = useState(false);
+  const [userAvatarURI, setUserAvatarURI] = useState<string>(
+    "https://github.com/nihilboy1.png"
+  );
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setAvatarIsLoading(true);
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (!result.canceled && result.assets[0].uri) {
+        const uri = result.assets[0].uri;
+        const photoInfo = await FileSystem.getInfoAsync(uri);
+        const photoInMB = photoInfo.size && photoInfo.size / 1024 / 1024;
+
+        console.log(photoInMB);
+
+        if (photoInMB && photoInMB > 3) {
+          return toast.show({
+            title: "Essa imagem é muito grande, escolha uma de até 3MB",
+            placement: "top",
+            bgColor: "red.500",
+            width: "85%",
+            alignSelf: "center",
+            _title: { textAlign: "center" },
+          });
+        }
+        setUserAvatarURI(uri);
+      }
+
+      return;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setAvatarIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt="6" px="10">
-          {avatarIsLoading ? (
-            <Skeleton
-              w={PHOTO_SIZE}
-              h={PHOTO_SIZE}
-              rounded="full"
-              startColor="gray.400"
-              endColor="gray.600"
-            />
-          ) : (
-            <Avatar
-              source={{ uri: "https://github.com/nihilboy1.png" }}
-              alt="Foto so usuário"
-              size={PHOTO_SIZE}
-            />
-          )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
+            {avatarIsLoading ? (
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.400"
+                endColor="gray.600"
+              />
+            ) : (
+              <Avatar
+                source={{ uri: userAvatarURI }}
+                alt="Foto so usuário"
+                size={PHOTO_SIZE}
+              />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="white" fontWeight="bold" fontSize="md" mt="2" mb="8">
               Alterar foto
             </Text>
           </TouchableOpacity>
+
           <Input placeholder="Nome" bg="gray.600" />
           <Input
             placeholder="Email"
